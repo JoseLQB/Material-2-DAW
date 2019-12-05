a3f3b00cea2f1b4457548a00d77a6ddd35114dc3
Jose L Quintanilla Blanco <44639383+JoseLQB@users.noreply.github.com>
2019-12-05 13:36:57 +0100

    Add files via upload

---
+++ DWC/Boletin5/ejer14.js
@@ -0,0 +1,45 @@
+window.onload=iniciar;
+function iniciar(){
+    document.getElementById("ok").addEventListener("click", checkCookie, false);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  
+}
+function getCookie(cname){
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function guardar() {
+}
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        alert("Hola " + username);
+    } else {
+        var nombre = document.getElementById("nombre").value;	 
+        var numero = document.getElementById("numero").value;
+        var cafe = document.getElementById("cafe").value;
+        var info = nombre + "-" + numero + "-" + cafe; 
+        if (nombre != "" && nombre != null) {
+            setCookie("username", info, 365);
+        }
+    }
+}
