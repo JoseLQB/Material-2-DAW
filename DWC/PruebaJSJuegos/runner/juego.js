e10f64280de4202a7a1e0d1b0e2c7327e16e079b
JoseLQB <josequb88@gmail.com>
2020-01-28 07:41:01 +0100

    j

--- DWC/PruebaJSJuegos/runner/juego.js
+++ DWC/PruebaJSJuegos/runner/juego.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){ 
-    $("buton").click(function(){
+    $("input").click(function(){
         //$("div").animate({fontSize: "140em",},2500);
-        
-        $("#azul").animate({left: "800px",},velocidad(1,5)*1000);
-        $("#rojo").animate({left: "800px",},velocidad(1,5)*1000);
-        $("#verde").animate({left: "800px",},velocidad(1,5)*1000);
-        $("#naranja").animate({left: "800px",},velocidad(1,5)*1000);
+        var vAzul = velocidad(1,5)*1000;
+        var vRojo = velocidad(1,5)*1000;
+        var vVerde = velocidad(1,5)*1000;
+        var vNaranja = velocidad(1,5)*1000;
+        $("#azul").animate({left: "800px",}, vAzul);
+        $("#azul").animate({left: "1600px",}, vVerde);
+        $("#rojo").animate({left: "800px",},vRojo);
+        $("#verde").animate({left: "800px",},vVerde);
+        $("#naranja").animate({left: "800px",},vNaranja);
+        //setInterval(ganador(vAzul, vVerde, vRojo, vNaranja), 50000);
     })
 });
 
@@ -13,8 +18,13 @@ function velocidad(a,b) {
     return Math.random()*(b-a)+parseInt(a);
 }
 
-function ganador(){
-    if($("div").css("left", "800px")){
-        alert("hola");
-    }
+function ganador(v1, v2, v3, v4){
+    var resultados = [v1, v2, v3, v4];
+    var may = 0;
+    resultados.forEach(e => {
+        if(e>may){
+            may = e;
+        }
+    });
+    alert(may);
 }
