55d4883ad4f41ee6aca904623a1a43ba15850b80
Jose L Quintanilla Blanco <44639383+JoseLQB@users.noreply.github.com>
2020-01-29 23:35:20 +0100

    d

---
+++ DWC/PruebaJSJuegos/Index/index.js
@@ -0,0 +1,10 @@
+
+
+$("document").ready(function(){
+    $('body').animate({
+        'background-position-x': '10000%',
+        'background-position-y': '10000%'
+    }, 100000, 'linear');
+    
+
+})
